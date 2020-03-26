d51666db9852c16d87b6a5071f2f82cc9bce27df
root <root@asdfsadfsadfsadf.tk>
2020-03-26 01:28:29 -0700

    update main

--- ofek/main.c
+++ ofek/main.c
@@ -8,7 +8,6 @@ int main() {
     int fd = tun_alloc(TUN_NAME);
 
     read(fd, c, 30);
-    printf("%d %d %d %d %d %d %d %d\n", c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]);
 
     scanf("%s", c);
     tun_close(fd);
