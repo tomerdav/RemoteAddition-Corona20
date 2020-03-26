9b3535b4fa6f97d1a2e42d07093bb5c0146801ae
root <root@asdfsadfsadfsadf.tk>
2020-03-26 01:28:08 -0700

    Add script

--- ofek/main.c
+++ ofek/main.c
@@ -7,6 +7,9 @@ int main() {
     char c[100];
     int fd = tun_alloc(TUN_NAME);
 
+    read(fd, c, 30);
+    printf("%d %d %d %d %d %d %d %d\n", c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]);
+
     scanf("%s", c);
     tun_close(fd);
 
